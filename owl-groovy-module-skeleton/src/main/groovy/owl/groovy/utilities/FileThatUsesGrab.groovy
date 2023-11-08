f80f8e8c246343e52b608879b9b01592f2070f55
Federico von Wernich <feddericokz @gmail.com>
2023-11-08 16:46:20 -0300

    Fix example

--- owl-groovy-module-skeleton/src/main/groovy/owl/groovy/utilities/FileThatUsesGrab.groovy
+++ owl-groovy-module-skeleton/src/main/groovy/owl/groovy/utilities/FileThatUsesGrab.groovy
@@ -1,6 +1,11 @@
 package owl.groovy.utilities
 
 // We point this to the local maven repository, to which we are publishing the library we're building with gradle.
+@GrabResolver(name='localMaven', root='/home/fedderico/.m2/repository/')
+
+@Grab(group='owl-utilities', module='owl-groovy-example', version='0.0.1-SNAPSHOT')
+
+import owl.groovy.utilities.OtherScriptFile
 
 // Script code:
 
